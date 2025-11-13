d7252a0b4d106722fda43e9d59bfa682c0619488
Tinny <mbothepha@gmail.com>
2025-11-13 14:29:11 +0200

    edit server and package manager

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
+/* ******************************************
+ * This is the application server
+ * ******************************************/
+const express = require("express")
+
+const app = express()
+/* ******************************************
+ * Default GET route
+ * ***************************************** */
+app.get("/", (req, res) => {res.send("Welcome home!")})
+
+/* ******************************************
+ * Server host name and port
+ * ***************************************** */
+const HOST = 'localhost'
+const PORT = 3000
+
+/* ***********************
+* Log statement to confirm server operation
+* *********************** */
+app.listen(PORT, () => {
+console.log(`moofar app listening on ${HOST}:${PORT}`)
+})
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
