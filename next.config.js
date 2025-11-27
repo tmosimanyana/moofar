e06a624a56dd9f6ce679439651f003ad8654bb8e
Tinny Mosimanyana <mbothepha@gmail.com>
2025-11-27 12:56:46 +0200

    update file

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // applies to all routes
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: process.env.VERCEL_ENV === 'production' ? 'index, follow' : 'noindex',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
